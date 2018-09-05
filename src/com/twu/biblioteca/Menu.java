7f771f95e8262ed4c6539a48165bfcf6a4f83616
Isa <lajansa@hotmail.com>
2018-09-06 01:14:13 +0800

    added start menu method and test

--- src/com/twu/biblioteca/Menu.java
+++ src/com/twu/biblioteca/Menu.java
@@ -25,4 +25,36 @@ public class Menu {
         System.out.println("Type the number of an action you would like to take:");
         System.out.println(" ");
     }
+
+    public boolean checkWithinRange(String userInput) {
+        int userInputInt = Integer.parseInt(userInput);
+        if (userInputInt > 0 && userInputInt <= list.size()) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    public void start() {
+        boolean running = true;
+
+        printDescription();
+
+        while (running) {
+            printMenu();
+
+            Helper helper = new Helper();
+            String userInput = helper.getUserInput("What would you like to do? ").toLowerCase();
+
+            if (userInput.equals("q")) {
+                running = false;
+            } else if (userInput.equals("b")) {
+                System.out.println("This is the home page. Please select a valid menu option!");
+            } else if (checkWithinRange(userInput)) {
+                running = list.get(Integer.parseInt(userInput) - 1).start();
+            } else {
+                System.out.println("Please select a valid option!");
+            }
+        }
+    }
 }
--- test/com/twu/biblioteca/MenuTest.java
+++ test/com/twu/biblioteca/MenuTest.java
@@ -3,10 +3,14 @@ package com.twu.biblioteca;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
 
 import java.io.ByteArrayOutputStream;
 import java.io.PrintStream;
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collection;
 
 import static org.junit.Assert.*;
 
@@ -19,11 +23,21 @@ class CreateMockMenuOption implements MenuOption {
     }
 }
 
+@RunWith(Parameterized.class)
 public class MenuTest {
     private final ByteArrayOutputStream outContent = new ByteArrayOutputStream();
     private final PrintStream originalOut = System.out;
     private Menu mockMenu = new Menu();
     private CreateMockMenuOption mockMenuOption = new CreateMockMenuOption();
+
+    private String input;
+
+    private boolean expected;
+
+    public MenuTest(String input, boolean expected) {
+        this.input = input;
+        this.expected = expected;
+    }
 
     @Before
     public void setUpStreams() {
@@ -51,5 +65,18 @@ public class MenuTest {
     public void printMenu() {
         mockMenu.printMenu();
         assertEquals("1. Test Mock\n", outContent.toString());
+    }
+
+    @Parameterized.Parameters
+    public static Collection<Object[]> data() {
+        return Arrays.asList(new Object[][] {
+                {"1", true},
+                {"2", false}
+        });
+    }
+
+    @Test
+    public void checkWithinRange() {
+        assertEquals(expected, mockMenu.checkWithinRange(input));
     }
 }
