cf71c91146172cb3ba25ae8b8bd865dd8f535a43
Isa <lajansa@hotmail.com>
2018-09-11 15:22:21 +0800

    added name, year, director and rating

--- src/com/twu/biblioteca/Movie.java
+++ src/com/twu/biblioteca/Movie.java
@@ -1,4 +1,40 @@
 package com.twu.biblioteca;
 
 public class Movie {
+    private String title;
+    private String yearReleased;
+    private String director;
+    private String rating = "unrated";
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getYearReleased() {
+        return yearReleased;
+    }
+
+    public void setYearReleased(String yearReleased) {
+        this.yearReleased = yearReleased;
+    }
+
+    public String getDirector() {
+        return director;
+    }
+
+    public void setDirector(String director) {
+        this.director = director;
+    }
+
+    public String getRating() {
+        return rating;
+    }
+
+    public void setRating(String rating) {
+        this.rating = rating;
+    }
 }
--- test/com/twu/biblioteca/MovieTest.java
+++ test/com/twu/biblioteca/MovieTest.java
@@ -1,5 +1,35 @@
+package com.twu.biblioteca;
+
+import org.junit.Test;
+
 import static org.junit.Assert.*;
 
 public class MovieTest {
+    Movie mockMovie = new Movie();
+
+    @Test
+    public void getTitle() {
+        mockMovie.setTitle("Mock Movie");
+        assertEquals("Mock Movie", mockMovie.getTitle());
+    }
+
+    @Test
+    public void getYearReleased() {
+        mockMovie.setYearReleased("2018");
+        assertEquals("2018", mockMovie.getYearReleased());
+    }
+
+    @Test
+    public void getDirector() {
+        mockMovie.setDirector("Mock Director");
+        assertEquals("Mock Director", mockMovie.getDirector());
+    }
+
+    @Test
+    public void getRating() {
+        assertEquals("unrated", mockMovie.getRating());
+    }
+
+
 
 }
