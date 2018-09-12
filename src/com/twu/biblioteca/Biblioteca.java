b52259dbf60535e04379bc139cdb197df5227984
Isa <lajansa@hotmail.com>
2018-09-13 01:15:54 +0800

    added login class

--- src/com/twu/biblioteca/Biblioteca.java
+++ src/com/twu/biblioteca/Biblioteca.java
@@ -5,11 +5,14 @@ public class Biblioteca {
     private Menu menu = new Menu();
     private BookList bookList = new BookList();
     private MovieList movieList = new MovieList();
+    private ArrayList<User> userList = new ArrayList<User>();
+    private Login login = new Login(userList, menu);
     private boolean running = true;
 
     public Biblioteca () {
         initialiseBookList();
         initialiseMovieList();
+        initialiseUserList();
         initialiseMenu();
     }
 
@@ -39,10 +42,30 @@ public class Biblioteca {
         }
     }
 
+    public void initialiseUserList() {
+        ArrayList<String> nameList = new ArrayList<String>(Arrays.asList("Isa", "Jason"));
+        ArrayList<String> emailList = new ArrayList<String>(Arrays.asList("isa@mail.com", "jason@mail.com"));
+        ArrayList<String> numberList = new ArrayList<String>(Arrays.asList("98765432", "91234567"));
+        ArrayList<String> roleList = new ArrayList<String>(Arrays.asList("librarian", "customer"));
+        ArrayList<String> libraryNumberList = new ArrayList<String>(Arrays.asList("987-9876", "123-1234"));
+        ArrayList<String> passwordList = new ArrayList<String>(Arrays.asList("librarian", "customer"));
+        for (int i = 0; i < nameList.size(); i++) {
+            User user = new User();
+            user.setName(nameList.get(i));
+            user.setEmail(emailList.get(i));
+            user.setNumber(numberList.get(i));
+            user.setRole(roleList.get(i));
+            user.setLibraryNumber(libraryNumberList.get(i));
+            user.setPassword(passwordList.get(i));
+            user.setLoginStatus(false);
+            userList.add(user);
+        }
+    }
+
     public void initialiseMenu() {
         menu.addMenuOption(bookList);
         menu.addMenuOption(movieList);
-
+        menu.addMenuOption(login);
     }
 
     public void printWelcomeMessage() {
--- src/com/twu/biblioteca/Login.java
+++ src/com/twu/biblioteca/Login.java
@@ -1,4 +1,74 @@
 package com.twu.biblioteca;
 
-public class Login {
+import java.util.ArrayList;
+
+public class Login implements MenuOption {
+    private ArrayList<User> userList;
+    private Menu menu;
+
+    public Login(ArrayList<User> userList, Menu menu) {
+        this.userList = userList;
+        this.menu = menu;
+    }
+
+    public String getMenuOptionTitle() {
+        return "Login";
+    }
+
+    public void printDescription() {
+        System.out.println("Login");
+        System.out.println("-----");
+        System.out.println(" ");
+    }
+
+    public boolean checkUserInput(String userInput) {
+        if (userInput.equals("q") || userInput.equals("b")) {
+            return false;
+        } else if (userInput.equals("login")) {
+            return checkCredentials();
+        } else {
+            return true;
+        }
+    }
+
+    public boolean checkCredentials() {
+        Helper helper = new Helper();
+        System.out.println("Please enter the following:");
+        String userLibraryNumber = helper.getUserInput("Library Number (xxx-xxxx): ");
+        String userPassword = helper.getUserInput("Password: ");
+        for (int i = 0; i < userList.size(); i++) {
+            if (userList.get(i).getLibraryNumber().equals(userLibraryNumber) && userList.get(i).getPassword().equals(userPassword)) {
+                Profile profile = new Profile(userList.get(i));
+                menu.addMenuOption(profile);
+                System.out.println("Logged in successfully!");
+                System.out.println(" ");
+                return true;
+            }
+        }
+        return true;
+    }
+
+
+    public boolean start() {
+
+        boolean running = true;
+
+        String userInput = null;
+
+        printDescription();
+
+        while (running) {
+
+            Helper helper = new Helper();
+            userInput = helper.getUserInput("What would you like to do? (login/q/b) ").toLowerCase();
+            running = checkUserInput(userInput);
+
+        }
+
+        if (userInput.equals("q")) {
+            return false;
+        } else {
+            return true;
+        }
+    }
 }
