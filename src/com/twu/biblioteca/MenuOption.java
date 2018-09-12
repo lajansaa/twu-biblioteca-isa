66f9dbc23bbc5f6a133f82acbb03fbbd688f5475
Isa <lajansa@hotmail.com>
2018-09-13 01:13:04 +0800

    added abstract methods to interface

--- src/com/twu/biblioteca/MenuOption.java
+++ src/com/twu/biblioteca/MenuOption.java
@@ -2,5 +2,7 @@ package com.twu.biblioteca;
 
 public interface MenuOption {
     abstract String getMenuOptionTitle();
+    abstract void printDescription();
     abstract boolean start();
+    abstract boolean checkUserInput(String userInput);
 }
