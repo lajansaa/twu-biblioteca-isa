68cfc34ff08f7136ef4bf5ba2d02735fafad741e
Isa <lajansa@hotmail.com>
2018-09-20 17:43:04 +0800

    created a class that handles currently logged in user

---
+++ src/com/twu/biblioteca/LoggedInUser.java
@@ -0,0 +1,14 @@
+package com.twu.biblioteca;
+
+public class LoggedInUser {
+    private User loggedInUser = null;
+
+    public void setLoggedInUser(User loggedInUser) {
+        this.loggedInUser = loggedInUser;
+    }
+
+    public User getLoggedInUser() {
+        return loggedInUser;
+    }
+
+}
